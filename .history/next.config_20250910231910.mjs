a087e2276eecd48c51e24e501706d181ed1d949c
xaidenlabs <alfreddadieng18@outlook.com>
2025-09-10 23:23:31 +0100

    correction

--- .history/next.config_20250910231910.mjs
+++ .history/next.config_20250910231910.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsHmrCache: false, // defaults to true
+    serverComponentsHmrCache: false,
   },
   images: {
     remotePatterns: [
