e3cb971bb366901ead03ea4fc96f07bd23603e99
xaidenlabs <alfreddadieng18@outlook.com>
2025-09-11 13:30:44 +0100

    updated

--- .history/app/(admin)/admin/cars/create/_components/add-car-form_20250723103559.jsx
+++ .history/app/(admin)/admin/cars/create/_components/add-car-form_20250723103559.jsx
@@ -363,7 +363,7 @@ export const AddCarForm = () => {
 
                   {/* Price */}
                   <div className="space-y-2">
-                    <Label htmlFor="price">Price (₦)</Label>
+                    <Label htmlFor="price">Price ($)</Label>
                     <Input
                       id="price"
                       {...register("price")}
